728e8f2a593718c3605d77414c9586067a2209cd
MoniqueMaquiné <MoniqueMaquin@10.150.140.24>
2018-05-29 12:19:42 +0200

    testmo3

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ hoi test
 fghrehgzdshzedg
 
 Hallo, ik ben monique, ik snap er niks van
+
 Stuart lukt het niet, ja je mist de context
--- src/Minigames.java
+++ src/Minigames.java
@@ -1,4 +1,11 @@
 
-public class Minigames {
-
+public class Minigames extends JFrame {
+	public static void main( String args[] ) {
+		JFrame frame = new Minigames();
+		frame.setBounds( 200, 200, 200, 200 );
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		frame.setTitle( "Mini Games" );
+		frame.setContentPane( new MiniGameView() );
+		frame.setVisible( true );
+	}
 }
