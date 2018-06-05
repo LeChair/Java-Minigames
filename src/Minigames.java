8f67dff97e85648249c6c9898ec4eac7f3430562
Jorrick <j.houtman@hotmail.nl>
2018-06-05 10:32:14 +0200

    Size aangepast zodat iris kan inporten ( sorry jasper )

--- src/Minigames.java
+++ src/Minigames.java
@@ -1,13 +1,12 @@
-import javax.swing.*;
+import javax.swing.JFrame;
 
 public class Minigames extends JFrame {
 	public static void main( String args[] ) {
 		JFrame frame = new Minigames();
-		frame.setSize(500, 500);
+		frame.setSize(600, 500);
 		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		frame.setTitle( "Mini Games" );
-		frame.setContentPane( new MinigameController() );
+		frame.setContentPane( new MinigameView() );
 		frame.setVisible( true );
-		frame.setLocation(400, 100);
 	}
 }
