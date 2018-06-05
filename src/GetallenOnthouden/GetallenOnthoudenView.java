120284ed119e5a872a1862c5462c882705f8aa61
Iris <iris.jansen@student.aventus.nl>
2018-06-05 12:05:42 +0200

    timer in view gezet

--- src/GetallenOnthouden/GetallenOnthoudenView.java
+++ src/GetallenOnthouden/GetallenOnthoudenView.java
@@ -1,13 +1,20 @@
 package GetallenOnthouden;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 public class GetallenOnthoudenView extends JPanel {
 	private GetallenOnthoudenModel getallenOnthoudenModel;
 	public JTextField invoervak;
 	public JLabel teOnthoudenGetalLabel;
+	private JLabel tijdlabel, tijdbeschrijving;
+	private Timer timer = new Timer(10, new TimerHandler());
+	LocalTime startTime=LocalTime.now();
+	LocalTime currentTime=LocalTime.now();
+	private boolean isgestart=false;
 	
 	public GetallenOnthoudenView(){
 		
@@ -19,6 +26,34 @@ public class GetallenOnthoudenView extends JPanel {
 		
 		add(invoervak);
 		add(teOnthoudenGetalLabel);
+
+		add(tijdbeschrijving);
+		add(tijdlabel);
+
+		if (!isgestart) {
+			timer.start();
+			startTime = LocalTime.now();
+			isgestart=true;
+		}
+		tijdlabel = new JLabel (""+timer);
+		tijdbeschrijving = new JLabel("Verstreken tijd:");
+
+
+
+	}
+
+	class TimerHandler implements ActionListener {
+		public void actionPerformed(ActionEvent e){
+			currentTime = LocalTime.now();
+			long elapsedtime= ChronoUnit.MILLIS.between(startTime, currentTime);
+
+			long minuten = elapsedtime / 60000;
+			long seconden = elapsedtime%60000;
+//            long milliseconden = elapsedtime/1000;
+
+			tijdlabel.setText("" + minuten + ":"+ seconden/1000 + " m:s");
+//            tijdlabel.setText(String.format("%2.2f", (double)(elapsedtime/1000.0))+" seconden" );
+		}
 	}
 
 	//timer maken
