5b93d6c1a57314cc6ee185b0056366a9ad849309
Iris <iris.jansen@student.aventus.nl>
2018-06-07 12:43:30 +0200

    invoerview: handler werkt, ingevoerde tekst zichtbaar in console

--- src/GetallenOnthouden/GetallenOnthoudenController.java
+++ src/GetallenOnthouden/GetallenOnthoudenController.java
@@ -8,6 +8,7 @@ import javax.swing.border.Border;
 
 public class GetallenOnthoudenController extends JPanel {
 	private GetallenOnthoudenView getallenRadenView;
+	private InvoerView invoerview;
     public JButton startgame;
     public int gamescreen = 0;
 	
@@ -15,21 +16,29 @@ public class GetallenOnthoudenController extends JPanel {
 		setLayout(new BorderLayout());
 
         startgame = new JButton("Start spel");
-        startgame.addActionListener(new ScreenHandler());
+        startgame.addActionListener(new Screen2Handler());
 
-        getallenRadenView = new GetallenOnthoudenView();
-
-        if(gamescreen==0) {
-            add(startgame, BorderLayout.NORTH);
-        }
+        add(startgame, BorderLayout.NORTH);
 
 	}
-    class ScreenHandler implements ActionListener {
+
+    class Screen2Handler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             gamescreen = 1;
+            getallenRadenView = new GetallenOnthoudenView();
             add(getallenRadenView, BorderLayout.CENTER);
             System.out.println(gamescreen);
             remove(startgame);
+        }
+    }
+
+    class Screen3Handler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            gamescreen = 2;
+            invoerview = new InvoerView();
+            add(invoerview, BorderLayout.CENTER);
+            System.out.println(gamescreen);
+            remove(getallenRadenView);
         }
     }
 }
--- src/GetallenOnthouden/InvoerView.java
+++ src/GetallenOnthouden/InvoerView.java
@@ -1,6 +1,31 @@
 package GetallenOnthouden;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class InvoerView extends JPanel {
+    public JTextField getalinvoer;
+    public JLabel getalvraag, controletekst;
+
+    public InvoerView(){
+
+        getalvraag = new JLabel("Wat was het nummer?");
+
+        getalinvoer = new JTextField(10);
+        getalinvoer.addActionListener(new CheckHandler());
+
+        controletekst = new JLabel("Druk op enter om te controleren");
+
+        add(getalvraag);
+        add(getalinvoer);
+        add(controletekst);
+    }
+
+    class CheckHandler implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            String invoer = getalinvoer.getText();
+            System.out.println(invoer);
+        }
+    }
 }
